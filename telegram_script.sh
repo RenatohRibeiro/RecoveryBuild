8b1386eb35ead8bc1fefe8a0ad5e14493abd7e7b
Renatoh <49354525+RenatohRibeiro@users.noreply.github.com>
2020-09-02 14:23:31 -0300

    Fix

--- telegram_script.sh
+++ telegram_script.sh
@@ -9,7 +9,7 @@ if [ -z $TOKEN ]; then
 echo -e "Enter a bot token"
 exit 0
 fi
-sed -i "s/enzomacaco/$TOKEN/" telegram.sh
+sed -i "s/enzomacaco/$TOKEN/" telegram
 
 printf "Enter the chat ID you want the bot to send the build information to\nID: "
 read ID
@@ -18,6 +18,6 @@ if [ -z $ID ]; then
 echo -e "Enter the chat ID you want the bot to send the build information to"
 exit 0
 fi
-sed -i "s/enso/$ID/" telegram.sh
+sed -i "s/enso/$ID/" telegram
 
 sudo install telegram /usr/bin
